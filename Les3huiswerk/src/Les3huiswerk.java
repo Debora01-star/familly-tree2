d57ced5e18eae91ccfb91e4210e247770e60f660
Debora01-star <113106792+Debora01-star@users.noreply.github.com>
2022-09-20 21:43:08 +0200

    huiswerk deel 1

---
+++ Les3huiswerk/src/Les3huiswerk.java
@@ -0,0 +1,37 @@
+public class Les3huiswerk {
+
+    public static void main(String[] args) {
+
+
+        System.out.println("Welcome to lesson 3");
+
+        String numberNumeric[] = {"1", "2", "3", "4", "5", "6", "7", "8", "9", "0"};
+        String lettersAlphabetic[] = {"een", "twee", "drie", "vier", "vijf", "zes", "zeven", "acht", "negen", "nul"};
+
+        int numbersNumeric[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 0};
+
+        System.out.println(numberNumeric[0]);
+        System.out.println(numberNumeric[1]);
+        System.out.println(numberNumeric[2]);
+        System.out.println(numberNumeric[3]);
+        System.out.println(numberNumeric[4]);
+        System.out.println(numberNumeric[5]);
+        System.out.println(numberNumeric[6]);
+        System.out.println(numberNumeric[7]);
+        System.out.println(numberNumeric[8]);
+        System.out.println(numberNumeric[9]);
+
+        System.out.println(lettersAlphabetic[0]);
+        System.out.println(lettersAlphabetic[1]);
+        System.out.println(lettersAlphabetic[2]);
+        System.out.println(lettersAlphabetic[3]);
+        System.out.println(lettersAlphabetic[4]);
+        System.out.println(lettersAlphabetic[5]);
+        System.out.println(lettersAlphabetic[6]);
+        System.out.println(lettersAlphabetic[7]);
+        System.out.println(lettersAlphabetic[8]);
+        System.out.println(lettersAlphabetic[9]); }
+
+        }
+
+
